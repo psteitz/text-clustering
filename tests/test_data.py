801a18f9b9f2ce8300941a924ba5a45399bcdb7d
Phil Steitz <phil.steitz@gmail.com>
2023-12-12 10:53:30 -0700

    Add more tests.

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,3 +1,6 @@
+"""
+Test data loading from csv to hf dataset.
+"""
 import context
 from data import csv_to_hf
 from datasets import load_from_disk
@@ -7,7 +10,8 @@ assert ds["text"][0] == "apple1"
 assert ds.num_rows == 4
 csv_to_hf.convert_csv_to_hf("test_load.csv", "Duck", "test_data")
 ds = load_from_disk("test_data")
-assert ds["text"][0] is None
-assert ds["text"][1] == "duck2"
-assert ds.num_rows == 4
+assert ds["text"][0] == "duck2"
+assert ds["text"][1] == "duck3"
+assert ds.num_rows == 3
+assert ds.num_columns == 1
 print(ds)
