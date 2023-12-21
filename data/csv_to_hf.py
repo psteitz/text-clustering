9ded8299c5cc8396734004c49fa14454fd888030
Phil Steitz <phil.steitz@gmail.com>
2023-12-20 17:54:06 -0700

    Add some methods.

--- data/csv_to_hf.py
+++ data/csv_to_hf.py
@@ -11,6 +11,25 @@ from datasets import Dataset
 import pandas as pd
 
 
+def _get_df(csv_path: str) -> pd.DataFrame:
+    """
+    Get pandas dataframe from csv file, handling decoding errors.
+
+    Args:
+        csv_path (str): Path to csv file containing text to cluster.
+
+    Returns:
+        pd.DataFrame: Dataframe containing text to cluster.
+    """
+    # read csv file
+    # if a decoding error occurs, try adding encoding="cp1252" (Windows encoding)
+    try:
+        df = pd.read_csv(csv_path)
+    except UnicodeDecodeError:
+        df = pd.read_csv(csv_path, encoding="cp1252")
+    return df
+
+
 def convert_csv_to_hf(csv_path, text_column, hf_name):
     """
     Convert csv file to huggingface dataset set up for nli task.
@@ -22,7 +41,8 @@ def convert_csv_to_hf(csv_path, text_column, hf_name):
         hf_name (str): Name of huggingface dataset to create.
     """
     # read csv file
-    df = pd.read_csv(csv_path)
+    # if a decoding error occurs, try adding encoding="cp1252" (Windows encoding)
+    df = _get_df(csv_path)
     # drop columns other than text column
     df = df.drop(df.columns.difference([text_column]), axis=1)
     # drop rows with empty text
@@ -33,6 +53,22 @@ def convert_csv_to_hf(csv_path, text_column, hf_name):
     dataset = dataset.rename_column(text_column, "text")
     # save dataset
     dataset.save_to_disk(hf_name)
+
+
+def get_column_names(csv_path: str) -> list:
+    """
+    Get column names from csv file.
+
+    Args:
+        csv_path (str): Path to csv file containing text to cluster.
+
+    Returns:
+        list: List of column names.
+    """
+    # read csv file
+    df = _get_df(csv_path)
+    # get column names
+    return list(df.columns)
 
 
 def main():
